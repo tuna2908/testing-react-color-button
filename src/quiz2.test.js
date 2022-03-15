e5bc01b8d22318d1efea823abd7aab9e5ebfed2f
ohohohjkl <anhtuck01@gmail.com>
2022-03-15 15:47:37 +0700

    update content

--- src/quiz2.test.js
+++ src/quiz2.test.js
@@ -1,13 +1,49 @@
-import { fireEvent } from "@testing-library/react";
-import App from "./App";
-import { initButtonAndCheckBoxStatus } from "./App.test";
-import { COLOR_NAMES } from "./common/constants";
+import { fireEvent } from '@testing-library/react';
+import App from './App';
+import { initButtonAndCheckBoxStatus } from './App.test';
+import { COLOR_NAMES } from './common/constants';
 
 /**
  * QUIZ 2: BUTTON TURNS GRAY
  */
 
-test("check if button turns gray", () => {
+test('check if button turns gray', () => {
+  const { colorButton, checkbox } = initButtonAndCheckBoxStatus();
+
+  //disable button 1st time
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  //check button disable
+  expect(colorButton).toHaveStyle({ backgroundColor: COLOR_NAMES.GRAY });
+
+  //enable button
+  fireEvent.click(checkbox);
+
+  //click button
+  fireEvent.click(colorButton);
+
+  //checkbutton turn red
+  expect(colorButton).toHaveStyle({ backgroundColor: COLOR_NAMES.RED });
+
+  //change color
+  fireEvent.click(colorButton);
+
+  //disable button
+  fireEvent.click(checkbox);
+
+  //check button disable
+  expect(colorButton).toHaveStyle({ backgroundColor: COLOR_NAMES.GRAY });
+
+  //enable
+  fireEvent.click(checkbox);
+
+  //check button turn blue
+  expect(colorButton).toHaveStyle({
+    backgroundColor: COLOR_NAMES.MIDNIGHT_BLUE,
+  });
+});
+
+test('check if button turns gray', () => {
   const { colorButton, checkbox } = initButtonAndCheckBoxStatus();
   //disable Button 1st
   fireEvent.click(checkbox);
